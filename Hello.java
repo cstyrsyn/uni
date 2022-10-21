019bc0dd868dbca918bff4845b3341b4f0ef392d
cstyrsyn <94677906+cstyrsyn@users.noreply.github.com>
2022-10-21 12:24:48 +1100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-  public static voic main(String[] args) {
+  public static void main(String[] args) {
   // displays "Hello Everyone" in console
   System.out.println("Hello Everyone");
   }
